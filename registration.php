80d8e48f2ca03b390776b493d37ec168a2a568c2
Ayah Awadallah <59929244+ayah-a99@users.noreply.github.com>
2020-02-18 17:14:43 -0500

    Add files via upload
    Updated all files

---
+++ registration.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+header('location:login.html');
+
+$con = mysqli_connect('localhost','root','root1234');
+
+mysqli_select_db($con, 'userregistration');
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$s = " select * from users where email = '$email'";
+
+$result = mysqli_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1) {
+	echo "Email is already in use.";
+}
+else {
+	$reg = " insert into users(email, password) values ('$email', '$password') ";
+	mysqli_query($con, $reg);
+	echo "Registration successful!";
+}
+
+?>
---
+++ validation.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+$con = mysqli_connect('localhost','root','root1234');
+
+mysqli_select_db($con, 'userregistration');
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$s = " select * from users where email = '$email' && password ='$password'";
+
+$result = mysqli_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1) {
+	//$_SESSION['email'] = $email;
+	header('location:main.html');
+}
+else {
+	header('location:login.html');
+}
+
+?>

ac16514c4affb8cb43c0c393c26e26615ee2fafc
Hafsa Hussain <47435957+hafsa-hussain@users.noreply.github.com>
2020-03-25 14:31:11 -0400

    Utilizing abstract factory design pattern
    I chose the registration functionality to display the abstract factory design pattern.

--- registration.php
+++ registration.php
@@ -1,14 +1,17 @@
 <?php
 
 session_start();
-header('location:login.html');
 
-$con = mysqli_connect('localhost','root','root1234');
+
+$con = mysqli_connect('localhost','root','');
 
 mysqli_select_db($con, 'flipbook');
 
 $email = $_POST['email'];
 $password = $_POST['password'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$password2 = $_POST['password2'];
 
 $s = " select * from users where email = '$email'";
 
@@ -16,13 +19,41 @@ $result = mysqli_query($con, $s);
 
 $num = mysqli_num_rows($result);
 
-if($num == 1) {
-	echo "Email is already in use.";
+if($password != $password2){
+	$_SESSION['valid_password'] = false;
+	//echo "Passwords do not match!";
+	header('location:signUpPage.php');
+}
+else if($num == 1) {
+	$_SESSION['valid_email'] = false;
+	header("location:signUpPage.php");
 }
 else {
-	$reg = " insert into users(email, password) values ('$email', '$password') ";
+	$reg = " insert into users(email, password, fname, lname) values ('$email', '$password', '$fname', '$lname') ";
 	mysqli_query($con, $reg);
 	echo "Registration successful!";
+	header('location:login.php');
 }
+
+
+
+/*if($num == 1) {
+	$_SESSION['valid_email'] = true;
+	echo "Email is already in use.";
+	
+}
+else {
+	if ($password == $password2) {
+		$reg = " insert into users (email, password, fname, lname) values ('$email', '$password', '$fname', '$lname')";
+		mysqli_query($con, $reg);
+		echo "Registration successful!";
+		header('location:login.php');
+	}
+	else {
+		echo"<script type='text/javascript'>alert('Passwords do not match.');</script>";
+		header("Refresh:0 url=signUpPage.php");
+	}
+
+}*/
 
 ?>

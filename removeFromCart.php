8a706b35828c9180efdbd553bc13a8a9e1d42885
William Aman <60115899+williamtaman@users.noreply.github.com>
2020-04-12 18:34:56 -0400

    Upload of removeFromCart.php
    This is where checkout.php redirects to when "Remove book" is selected. It handles the back-end removal of the book from the cart table

---
+++ removeFromCart.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+$con = mysqli_connect('localhost','root','root1234');
+
+mysqli_select_db($con, 'flipbook');
+
+$email = $_SESSION['email'];
+$bookid = $_POST['specificBook'];
+
+mysqli_query($con,"DELETE FROM cart WHERE bookid = '$bookid' AND email = '$email'");
+
+header("location:checkout.php");
+?>

a0d0d11099be637959a81e35ebd10a99cfd0edd0
William Aman <60115899+williamtaman@users.noreply.github.com>
2020-04-12 18:47:52 -0400

    Update removeFromCart.php
    Changed session variable name from email to user to match the other files.

--- removeFromCart.php
+++ removeFromCart.php
@@ -6,7 +6,7 @@ $con = mysqli_connect('localhost','root','root1234');
 
 mysqli_select_db($con, 'flipbook');
 
-$email = $_SESSION['email'];
+$email = $_SESSION['user'];
 $bookid = $_POST['specificBook'];
 
 mysqli_query($con,"DELETE FROM cart WHERE bookid = '$bookid' AND email = '$email'");

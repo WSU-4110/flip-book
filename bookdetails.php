58024962fe19b79a186f16797237c94a1833c11e
Hafsa Hussain <47435957+hafsa-hussain@users.noreply.github.com>
2020-04-13 18:39:04 -0400

    Changing formatting of book details page to table layout

--- bookdetails.php
+++ bookdetails.php
@@ -25,6 +25,7 @@
 			$classNum = $row['classNum'];
 			$price = $row['price'];
 			$postUser = $row['email'];
+			$img = $row['img'];
 			
 			$userQuery = " SELECT * FROM users WHERE email = '$postUser' ";
 			$userQuery_result = mysqli_query($con, $userQuery);
@@ -41,15 +42,48 @@
 		}
 		
 			$output .= '<h1><center><strong>Book Details</strong></center></h1>
-					<h2>
-					<center><div> <b>Posted by: </b>'.$fname." ".$lname.'</div></center>
-					<center><div> <b>Book Title: </b>'.$bookTitle.'</div></center>
-					<center><div> <b>Author: </b>'.$author.'</div></center>
-					<center><div> <b>ISBN: </b>'.$ISBN.'</div></center>
-					<center><div> <b>Edition: </b>'.$edition.'</div></center>
-					<center><div> <b>Subject: </b>'.$subject.'</div></center>
-					<center><div> <b>Class Number: </b>'.$classNum.'</div></center>
-					<center><div> <b>Price: </b>'.$price.'</div></center></h2>'; 
+					
+					<center>
+					<style>
+					table {
+					  font-family: arial, sans-serif;
+					  border-collapse: collapse;
+					  width: 100%;
+					}
+
+					td, th {
+					  border: 3px solid green;
+					  text-align: left;
+					  padding: 10px;
+					}
+
+					tr:nth-child(even) {
+					  background-color: #FFFFCC;
+					}
+					</style>
+					
+					<table>
+					  <tr>
+						<th><h2><center>Book</center></h2></th>
+						<th><h2><center>Details</center></h2></th>
+					  </tr>
+					  <tr>
+						<td><center><img width="300px" height="350px" src="images/'.$img.'" /></center></td>
+						<td>
+						<h2>
+						<div> <b>Posted by: </b>'.$fname." ".$lname.'</div>
+						<div> <b>Book Title: </b>'.$bookTitle.'</div>
+						<div> <b>Author: </b>'.$author.'</div>
+						<div> <b>ISBN: </b>'.$ISBN.'</div>
+						<div> <b>Edition: </b>'.$edition.'</div>
+						<div> <b>Subject: </b>'.$subject.'</div>
+						<div> <b>Class Number: </b>'.$classNum.'</div>
+						<div> <b>Price: </b>'.$price.'</div>
+						</h2>
+						</td>
+					</tr>
+					</table>
+					</center>'; 
 		print($output);
 	}
 	else{
